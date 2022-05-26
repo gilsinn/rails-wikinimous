db4ab572675ab24d54b1748140b1b210c58cb142
Gil Singh <gilsinghsg@gmail.com>
2022-05-26 08:33:42 +0800

    Saving

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+puts 'Creating 10 fake entries...'
+10.times do
+  article = Article.new(
+    title:    Faker::Movie.title,
+    content: "#{Faker::Movie.quote}",)
+  article.save!
+end
